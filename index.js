e9be315fb8396fa6e99f63be69b93e6a0e930af5
Bence Knáb <benceknab.iac@gmail.com>
2020-07-12 13:21:58 +0200

    Add React sub config

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
 module.exports = {
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-
-    "airbnb-typescript",
-    "airbnb/hooks",
-
-    "prettier",
-    "prettier/react",
-    "prettier/@typescript-eslint",
-
-    "./base.js",
+    "./react.js",
   ],
-
-  env: {
-    browser: true,
-  },
-
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-
-  plugins: ["react-hooks"],
-
-  rules: {
-    "react/prop-types": "off",
-    "react/jsx-props-no-spreading": "off",
-  },
 };
