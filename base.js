b92be66a80f039a78ae6d9a49300236436861f79
Bence Knáb <benceknab.iac@gmail.com>
2020-07-20 11:06:29 +0200

    Update allowed dev dependency imports

--- base.js
+++ base.js
@@ -22,6 +22,36 @@ module.exports = {
 
     "import/prefer-default-export": "off",
     "import/no-anonymous-default-export": "error",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "test/**",
+          "tests/**",
+          "spec/**",
+          "**/__tests__/**",
+          "**/__mocks__/**",
+          "test.{js,jsx,ts,tsx}",
+          "test-*.{js,jsx,ts,tsx}",
+          "**/*{.,_}{test,spec,stories}.{js,jsx,ts,tsx}",
+          "**/jest.config.js",
+          "**/jest.setup.js",
+          "**/vue.config.js",
+          "**/webpack.config.js",
+          "**/webpack.config.*.js",
+          "**/rollup.config.js",
+          "**/rollup.config.*.js",
+          "**/gulpfile.js",
+          "**/gulpfile.*.js",
+          "**/Gruntfile{,.js}",
+          "**/protractor.conf.js",
+          "**/protractor.conf.*.js",
+          "**/karma.conf.js",
+          "**/setupTests.ts",
+        ],
+        optionalDependencies: false,
+      },
+    ],
 
     "prettier/prettier": "error",
 
@@ -42,14 +72,14 @@ module.exports = {
     ],
     "@typescript-eslint/explicit-member-accessibility": ["error", { accessibility: "explicit" }],
     "@typescript-eslint/explicit-module-boundary-types": [
-      "error", 
-      { 
+      "error",
+      {
         allowArgumentsExplicitlyTypedAsAny: false,
         allowDirectConstAssertionInArrowFunctions: true,
         allowedNames: [],
         allowHigherOrderFunctions: true,
         allowTypedFunctionExpressions: true,
-      }
+      },
     ],
     "@typescript-eslint/member-ordering": "error",
     "@typescript-eslint/method-signature-style": "error",
