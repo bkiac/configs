018d48c084eef22ef9cdd9959bc7a61aa742ea25
Bence Knáb <benceknab.iac@gmail.com>
2020-07-21 09:09:28 +0200

    Refactor rules

--- base.js
+++ base.js
@@ -1,3 +1,8 @@
+const native = require("./rules/import");
+const theImport = require("./rules/import");
+const prettier = require("./rules/prettier");
+const typescript = require("./rules/import");
+
 module.exports = {
   parser: "@typescript-eslint/parser",
 
@@ -14,114 +19,9 @@ module.exports = {
   ],
 
   rules: {
-    "lines-between-class-members": ["error", "always", { exceptAfterSingleLine: true }],
-    "no-console": "error",
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-    "no-underscore-dangle": "off",
-    "no-void": "off",
-
-    "import/prefer-default-export": "off",
-    "import/no-anonymous-default-export": "error",
-    "import/no-extraneous-dependencies": [
-      "error",
-      {
-        devDependencies: [
-          "test/**",
-          "tests/**",
-          "spec/**",
-          "**/__tests__/**",
-          "**/__mocks__/**",
-          "test.{js,jsx,ts,tsx}",
-          "test-*.{js,jsx,ts,tsx}",
-          "**/*{.,_}{test,spec,stories}.{js,jsx,ts,tsx}",
-          "**/jest.config.js",
-          "**/jest.setup.js",
-          "**/vue.config.js",
-          "**/webpack.config.js",
-          "**/webpack.config.*.js",
-          "**/rollup.config.js",
-          "**/rollup.config.*.js",
-          "**/gulpfile.js",
-          "**/gulpfile.*.js",
-          "**/Gruntfile{,.js}",
-          "**/protractor.conf.js",
-          "**/protractor.conf.*.js",
-          "**/karma.conf.js",
-          "**/setupTests.{js,ts}",
-          "**/setupProxy.{js,ts}",
-        ],
-        optionalDependencies: false,
-      },
-    ],
-
-    "prettier/prettier": "error",
-
-    "@typescript-eslint/array-type": ["error", { default: "array-simple" }],
-    "@typescript-eslint/consistent-type-assertions": [
-      "error",
-      { assertionStyle: "as", objectLiteralTypeAssertions: "never" },
-    ],
-    "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
-    "@typescript-eslint/explicit-function-return-type": [
-      "error",
-      {
-        allowExpressions: false,
-        allowTypedFunctionExpressions: true,
-        allowHigherOrderFunctions: true,
-        allowConciseArrowFunctionExpressionsStartingWithVoid: true,
-      },
-    ],
-    "@typescript-eslint/explicit-member-accessibility": ["error", { accessibility: "explicit" }],
-    "@typescript-eslint/explicit-module-boundary-types": [
-      "error",
-      {
-        allowArgumentsExplicitlyTypedAsAny: false,
-        allowDirectConstAssertionInArrowFunctions: true,
-        allowedNames: [],
-        allowHigherOrderFunctions: true,
-        allowTypedFunctionExpressions: true,
-      },
-    ],
-    "@typescript-eslint/member-ordering": "error",
-    "@typescript-eslint/method-signature-style": "error",
-    "@typescript-eslint/naming-convention": "error",
-    "@typescript-eslint/no-base-to-string": "error",
-    "@typescript-eslint/no-confusing-non-null-assertion": "error",
-    "@typescript-eslint/no-dynamic-delete": "error",
-    "@typescript-eslint/no-extraneous-class": "error",
-    "@typescript-eslint/no-invalid-void-type": "error",
-    "@typescript-eslint/no-parameter-properties": "error",
-    "@typescript-eslint/no-require-imports": "error",
-    "@typescript-eslint/no-throw-literal": "error",
-    "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
-    "@typescript-eslint/no-unnecessary-condition": "error",
-    "@typescript-eslint/no-unnecessary-qualifier": "error",
-    "@typescript-eslint/no-unnecessary-type-arguments": "error",
-    // "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars-experimental": [
-      "error",
-      { ignoreArgsIfArgsAfterAreUsed: true },
-    ],
-    "@typescript-eslint/prefer-for-of": "error",
-    "@typescript-eslint/prefer-function-type": "error",
-    "@typescript-eslint/prefer-includes": "error",
-    "@typescript-eslint/prefer-literal-enum-member": "error",
-    "@typescript-eslint/prefer-nullish-coalescing": "error",
-    "@typescript-eslint/prefer-optional-chain": "error",
-    "@typescript-eslint/prefer-readonly": "error",
-    "@typescript-eslint/prefer-reduce-type-parameter": "error",
-    "@typescript-eslint/prefer-string-starts-ends-with": "error",
-    "@typescript-eslint/prefer-ts-expect-error": "error",
-    "@typescript-eslint/promise-function-async": "error",
-    "@typescript-eslint/require-array-sort-compare": "error",
-    "@typescript-eslint/restrict-plus-operands": "error",
-    "@typescript-eslint/restrict-template-expressions": [
-      "error",
-      { allowNumber: true, allowBoolean: true, allowNullish: false, allowAny: false },
-    ],
-    "@typescript-eslint/strict-boolean-expressions": "error",
-    "@typescript-eslint/switch-exhaustiveness-check": "error",
-    "@typescript-eslint/type-annotation-spacing": "error",
-    "@typescript-eslint/unified-signatures": "error",
+    ...native,
+    ...theImport,
+    ...typescript,
+    ...prettier,
   },
 };
---
+++ rules/native.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "lines-between-class-members": ["error", "always", { exceptAfterSingleLine: true }],
+  "no-console": "error",
+  "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+  "no-underscore-dangle": "off",
+  "no-void": "off",
+};
