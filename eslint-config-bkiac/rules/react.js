35a3114760fd7bffc55eb877ede0f1478171e472
Ben Knáb <bkiac@pm.me>
2022-01-03 11:01:40 +0100

    Remove prop types rules

--- eslint-config-bkiac/rules/react.js
+++ eslint-config-bkiac/rules/react.js
@@ -1,7 +1,8 @@
 module.exports = {
 	rules: {
-		"react/prop-types": "off",
-		"react/jsx-props-no-spreading": "off",
 		"react/jsx-key": ["error", {checkFragmentShorthand: true}],
+		"react/jsx-props-no-spreading": "off",
+		"react/prop-types": "off",
+		"react/require-default-props": "off",
 	},
 }
